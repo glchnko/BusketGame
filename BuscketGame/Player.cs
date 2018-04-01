75546e94572d652afa913f4c99d7070a9e2c09e7
Anna Galuchenko <ania@Annas-MacBook-Pro.local>
2018-04-02 01:20:54 +0300

    init commit

---
+++ BuscketGame/Player.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuscketGame 
+{
+    
+    public class PlayerRandom
+    {
+        int rangeMin;
+        int rangeMax;
+        //int guessNumber;
+
+        public PlayerRandom(int min, int max)
+        {
+            if (min >= max)
+            {
+                throw new ArgumentException("max must be grater than min");
+            }
+            rangeMin = min;
+            rangeMax = max + 1;
+        }
+
+        public int NextGuess()
+        {
+            Random rnd = new Random();
+            //-----
+            //int res;
+            //res = rnd.Next(rangeMin, rangeMax);
+            //return res;
+            // -----
+            //int res = rnd.Next(rangeMin, rangeMax);
+            //return res; 
+
+            return rnd.Next(rangeMin, rangeMax);        
+        }
+
+    }
+
+    public class PlayerSequencial 
+    {
+        int rangeMin;
+        int rangeMax;
+        int guess;
+
+        public PlayerSequencial(int min, int max) 
+        {
+            rangeMin = min;
+            rangeMax = max;
+            guess = min;
+           
+        }
+
+        public int NextGuess()
+        {
+            if(guess > rangeMax) {
+                throw new ApplicationException();
+            }
+
+            //int a = 1;
+            // int b = a + 1;
+            //a = a + 1;
+            //a = a + 1;
+            //a = a + 1;
+            //a += 1;
+            //a++;
+
+            int res = guess;
+
+            guess = guess + 1;
+
+            return res;
+
+         
+            //return guess++;
+        }
+    }
+
+    public class PlayerSmartRandom {
+
+        int rangeMin;
+        int rangeMax;
+        List<int> guesses = new List<int>();
+
+        public PlayerSmartRandom(int min, int max)
+        {
+            if (min >= max)
+            {
+                throw new ArgumentException("max must be grater than min");
+            }
+            rangeMin = min;
+            rangeMax = max + 1;
+
+        }
+
+        public int NextGuess()
+        {
+            Random rnd = new Random();
+            //-----
+            int res;
+
+
+            do
+            {
+                res = rnd.Next(rangeMin, rangeMax);
+
+            }
+            while (guesses.Contains(res));
+
+            guesses.Add(res);
+
+            return res;
+        }
+    }
+}
---
+++ BuscketGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BuscketGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter minValue = ");
+            int minValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter maxValue = ");
+            int maxValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter guessNumber = ");
+            int guessNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            PlayerSequencial player1 = new PlayerSequencial(minValue, maxValue);
+            PlayerRandom player2 = new PlayerRandom(minValue, maxValue);
+            PlayerSmartRandom player3 = new PlayerSmartRandom(minValue, maxValue);
+            int p1guess;
+            int p2guess;
+            int p3guess;
+            do
+            {
+                p1guess = player1.NextGuess();
+                Console.WriteLine("Player 1 (Seq) guess is: " + p1guess);
+                p2guess = player2.NextGuess();
+                Console.WriteLine("Player 2 (Rnd) guess is: " + p2guess);
+                p3guess = player3.NextGuess();
+                Console.WriteLine("Player 3 (SmartRnd) guess is: " + p3guess);
+
+                if (p1guess == guessNumber)
+                {
+                    Console.WriteLine("PlayerRandom won!");
+                    break;
+                }
+                else if (p2guess == guessNumber)
+                {
+                    Console.WriteLine("PlayerSequencial won!");
+                    break;
+                }
+                else if (p3guess == guessNumber)
+                {
+                    Console.WriteLine("PlayerSmartRandom won!");
+                    break;
+                }
+            }
+            while (true);
+          
+        }
+
+    }
+}
